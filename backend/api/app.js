a8883176156271445eda175f7cb17e3c5450eebc
Cemalll <c.gumus2021@gtu.edu.tr>
2025-10-31 15:21:34 +0300

    Dynamic Routing Added, Some Routes Added

--- backend/api/app.js
+++ backend/api/app.js
@@ -6,9 +6,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -19,11 +16,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); 
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.use('/api',require('./routes/index')) ;// dinamic route loading
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-const config = require('../config'); // ..config/index.js by default
+//const config = require('../config'); // ..config/index.js by default
+const config = require('../config');
+const fs = require('fs');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' , config});
+
+/* Dynamically load all route files in the current directory */
+let routes = fs.readdirSync(__dirname);
+routes.forEach(file => {
+    if (file !== 'index.js') {
+        const route = require(`./${file}`);
+        const routePath = `/${file.replace('.js', '')}`;
+        router.use(routePath, route);
+    }
 });
+
+
+/* Dynamically load all route files in the current directory */
+/*  Alternative syntax
+let routes = fs.readdirSync(__dirname);
+for (let route of routes) {
+  if (route !== 'index.js' && route.endsWith('.js')) {
+    router.use("/"+route.replace('.js',''), require(`./${route}`));
+  }
+}
+*/
+
+
 
 module.exports = router;
