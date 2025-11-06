e1a01905d2768ca1edfc8eb5a6fc5086eb0b2173
Burak Kurtaran <94144518+BUFURAK@users.noreply.github.com>
2025-11-06 22:37:14 +0300

    Update and rename frontend/CombineApp/screens/HomeScreen.js to frontend/CombineApp/screens/HomeScreen/HomeScreen.js

--- frontend/CombineApp/screens/HomeScreen/HomeScreen.js
+++ frontend/CombineApp/screens/HomeScreen/HomeScreen.js
@@ -9,7 +9,7 @@ import {
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { LinearGradient } from 'expo-linear-gradient';
-import { COLORS } from '../constants/colors';
+import { COLORS } from '../colors';
 import { Ionicons } from '@expo/vector-icons';
 
 // --- SİMÜLASYON VERİSİ ---
