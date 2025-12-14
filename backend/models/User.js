a2fd7ee04224e8486809f24f5afbc721ec75f029
Burak Kurtaran <94144518+BUFURAK@users.noreply.github.com>
2025-12-14 12:47:05 +0300

    Add reset password fields to User schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  resetPasswordCode: { type: String },
+  resetPasswordExpires: { type: Date },
 });
 
 const User = mongoose.model("User", userSchema);
