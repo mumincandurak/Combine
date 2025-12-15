248f3e1c9c73d9c3d4979dfe73cd76d87be9ed6e
Burak Kurtaran <94144518+BUFURAK@users.noreply.github.com>
2025-12-15 22:52:10 +0300

    Add name and profileImageUrl fields to user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
+  name: { type: String }, // Display name
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  profileImageUrl: { type: String },
   resetPasswordCode: { type: String },
   resetPasswordExpires: { type: Date },
 });
